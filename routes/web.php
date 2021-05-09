884b44ec6f799051674b414236ab0a0bf8517135
[Christian Odigbo] <christian.odigbo73@gmail.com>
2021-05-09 10:03:25 +0100

    New Files

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,10 @@ Route::get('/', 'HomePageController@home');
 Route::get('/contact', 'HomePageController@contact');
 Route::post('/contact_us', 'HomePageController@contact_us');
 Route::get('/blog', 'HomePageController@blog');
-<<<<<<< HEAD
 Route::get('/portfolio-details', 'HomePageController@portfolio_details');
 Route::get('/faq', 'HomePageController@faq');
-=======
 Route::get('/service-page-gadgets', 'HomePageController@service_page_gadgets');
 Route::get('/service-page-ITsolutions', 'HomePageController@service_page_ITsolutions');
 Route::get('/service-page-others', 'HomePageController@service_page_others');
 Route::get('/service-page-repairs', 'HomePageController@service_page_repairs');
 Route::get('/service-page-software', 'HomePageController@service_page_software');
->>>>>>> bd0bd27 (new-files)
